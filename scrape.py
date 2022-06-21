bd8391aef14ce37a8a98d45bdf18f0ec016ac7eb
Prateik Sinha <81702110+Prateik-11@users.noreply.github.com>
2022-06-21 11:11:02 -0700

    Committing files of first successful web scrape
    The notebook contains exploration of web scraping tools and the logic used to arrive at workflow

---
+++ scrape.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pandas as pd
